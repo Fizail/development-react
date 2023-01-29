2cb856f03765efa99304473784dcb09d37fce82f
Fizail <89292772+Fizail@users.noreply.github.com>
2023-01-29 09:30:05 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1",{id: 'title1',},  "This is my first react heading");
+const heading2 = React.createElement("h2",{id:'title2',},"This is second heading");
+const container = React.createElement("div",{id:'container',},[heading,heading2]);
+console.log(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
